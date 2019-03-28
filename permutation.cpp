29c19eeede890c8047e72553f267d36b9f91bbbc
胡玮文 <huww98@outlook.com>
2019-03-29 00:57:11 +0800

    Expansion function

--- permutation.cpp
+++ permutation.cpp
@@ -5,24 +5,24 @@
 
 using namespace std;
 
-constexpr auto buildIpTable() {
-	array<uint8_t, 64> ipTable{
-		58, 50, 42, 34, 26, 18, 10, 2,
-		60, 52, 44, 36, 28, 20, 12, 4,
-		62, 54, 46, 38, 30, 22, 14, 6,
-		64, 56, 48, 40, 32, 24, 16, 8,
-		57, 49, 41, 33, 25, 17, 9,  1,
-		59, 51, 43, 35, 27, 19, 11, 3,
-		61, 53, 45, 37, 29, 21, 13, 5,
-		63, 55, 47, 39, 31, 23, 15, 7,
-	};
-	for (auto& i : ipTable) {
+template<size_t size>
+constexpr auto minusOne(array<uint8_t, size> table) {
+    for (auto& i : table) {
 		i--;
 	}
-	return ipTable;
+    return table;
 }
 
-constexpr auto ipTable = buildIpTable();
+constexpr auto ipTable = minusOne<64>({
+	58, 50, 42, 34, 26, 18, 10, 2,
+	60, 52, 44, 36, 28, 20, 12, 4,
+	62, 54, 46, 38, 30, 22, 14, 6,
+	64, 56, 48, 40, 32, 24, 16, 8,
+	57, 49, 41, 33, 25, 17, 9,  1,
+	59, 51, 43, 35, 27, 19, 11, 3,
+	61, 53, 45, 37, 29, 21, 13, 5,
+	63, 55, 47, 39, 31, 23, 15, 7,
+});
 
 constexpr auto buildFpTable() {
 	array<uint8_t, 64> table{};
@@ -35,20 +35,43 @@ constexpr auto buildFpTable() {
 
 constexpr auto fpTable = buildFpTable();
 
-constexpr uint64_t slowPermutation(const array<uint8_t, 64> &permutationTable, uint64_t in) {
+constexpr auto eTable = minusOne<48>({
+    32, 1,  2,  3,  4,  5,
+    4,  5,  6,  7,  8,  9,
+    8,  9,  10, 11, 12, 13,
+    12, 13, 14, 15, 16, 17,
+    16, 17, 18, 19, 20, 21,
+    20, 21, 22, 23, 24, 25,
+    24, 25, 26, 27, 28, 29,
+    28, 29, 30, 31, 32, 1,
+});
+
+constexpr auto pTable = minusOne<32>({
+    16, 7,  20, 21, 29, 12, 28, 17,
+    1,  15, 23, 26, 5,  18, 31, 10,
+    2,  8,  24, 14, 32, 27, 3,  9,
+    19, 13, 30, 6,  22, 11, 4,  25,
+});
+
+/** Used to build mapping table only
+ */
+template<size_t outSize>
+constexpr uint64_t slowPermutation(const array<uint8_t, outSize> &permutationTable, uint64_t in) {
 	uint64_t result = 0;
-	for (size_t i = 0; i < 64; i++)
+	for (size_t i = 0; i < outSize; i++)
 	{
-		result |= in << (permutationTable[i]) & (uint64_t(1) << 63);
+		result |= (in << permutationTable[i]) & (uint64_t(1) << outSize - 1);
 		result >>= 1;
 	}
 	return result;
 }
 
-using PermutationMappingType = array<array<uint64_t, 256>, 8>;
+template<size_t inSize>
+using PermutationMappingType = array<array<uint64_t, 256>, inSize/8>;
 
-constexpr auto buildPermutationMapping(const array<uint8_t, 64> &permutationTable) {
-	PermutationMappingType mapping{};
+template<size_t outSize, size_t inSize>
+constexpr auto buildPermutationMapping(const array<uint8_t, outSize> &permutationTable) {
+	PermutationMappingType<inSize> mapping{};
 	for (size_t b = 0; b < mapping.size(); b++)
 	{
 		auto& m = mapping[b];
@@ -60,18 +83,23 @@ constexpr auto buildPermutationMapping(const array<uint8_t, 64> &permutationTabl
 	return mapping;
 }
 
-constexpr auto ipMapping = buildPermutationMapping(ipTable);
-constexpr auto fpMapping = buildPermutationMapping(fpTable);
+constexpr auto ipMapping = buildPermutationMapping<64,64>(ipTable);
+constexpr auto fpMapping = buildPermutationMapping<64,64>(fpTable);
+constexpr auto eMapping = buildPermutationMapping<48,32>(eTable);
 
-bitset<64> permutation(const PermutationMappingType &mapping, bitset<64> in)
+template<size_t outSize, size_t inSize>
+auto permutation(const PermutationMappingType<inSize> &mapping, bitset<inSize> in)
 {
-	bitset<64> result;
-	for (size_t i = 0; i < 8; i++)
+	bitset<outSize> result;
+	for (size_t i = 0; i < mapping.size(); i++)
 	{
 		result |= mapping[i][in.to_ulong() & 0xFF];
-	}
-	return result;
+        in >> 8;
+    }
+    return result;
 }
 
-bitset<64> ip(bitset<64> in) { return permutation(ipMapping, in); }
-bitset<64> fp(bitset<64> in) { return permutation(fpMapping, in); }
+bitset<64> ip(bitset<64> in) { return permutation<64>(ipMapping, in); }
+bitset<64> fp(bitset<64> in) { return permutation<64>(fpMapping, in); }
+
+bitset<48> e(bitset<32> in) { return permutation<48>(eMapping, in); }
--- permutation.h
+++ permutation.h
@@ -4,3 +4,4 @@
 
 std::bitset<64> ip(std::bitset<64> in);
 std::bitset<64> fp(std::bitset<64> in);
+std::bitset<48> e(std::bitset<32> in);
